cc48c9acd666d99812c23a5973716b39ffaaa679
udhungel <udeepdhungel@gmail.com>
2023-01-22 20:47:18 +0100

    Product API - Configure Db Context Added DbContext file Service configuration in the program.cs Connectionstring in appsetting.json

--- Mango.Services.Product/Program.cs
+++ Mango.Services.Product/Program.cs
@@ -1,3 +1,7 @@
+
+using Mango.Services.ProductAPI.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
