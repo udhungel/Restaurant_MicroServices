b6fd1817293c93e6ebe4f18ee7392e1ef7e042a5
udhungel <udeepdhungel@gmail.com>
2023-01-23 14:52:33 +0100

    ProductAPI - Seed Data to Product Table in DB

--- Mango.Services.Product/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.Product/DbContexts/ApplicationDbContext.cs
@@ -14,6 +14,52 @@ namespace Mango.Services.ProductAPI.DbContexts
 
         public DbSet<MangoProduct> Products { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<MangoProduct>().HasData(new MangoProduct 
+            {
+                ProductId = 1,
+                Name= "Samosa",
+                Price = 15,
+                Description = "Tasty snack food for Appetizer",
+                ImageUrl="",
+                CategoryName = "Appetizer"
+                
+            });
+            modelBuilder.Entity<MangoProduct>().HasData(new MangoProduct
+            {
+                ProductId = 2,
+                Name = "Paneer Tikka",
+                Price = 13.99,
+                Description = "Test meal for 2 ",
+                ImageUrl = "",
+                CategoryName = "Appetizer"
+
+            });
+            modelBuilder.Entity<MangoProduct>().HasData(new MangoProduct
+            {
+                ProductId = 3,
+                Name = "Sweet Pie",
+                Price = 10.99,
+                Description = "Dessert meal for 2 ",
+                ImageUrl = "",
+                CategoryName = "Dessert"
+
+            });
+            modelBuilder.Entity<MangoProduct>().HasData(new MangoProduct
+            {
+                ProductId = 4,
+                Name = "Pav Bhaji",
+                Price = 15,
+                Description = "Entree meal for 2 ",
+                ImageUrl = "",
+                CategoryName = "Entree"
+
+            });
+        }
+
 
     }
 }
---
+++ Mango.Services.Product/Migrations/20230123134142_SeedProducts.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mango.Services.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryName", "Description", "ImageUrl", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Appetizer", "Tasty snack food for Appetizer", "", "Samosa", 15.0 },
+                    { 2, "Appetizer", "Test meal for 2 ", "", "Paneer Tikka", 13.99 },
+                    { 3, "Dessert", "Dessert meal for 2 ", "", "Sweet Pie", 10.99 },
+                    { 4, "Entree", "Entree meal for 2 ", "", "Pav Bhaji", 15.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+        }
+    }
+}
