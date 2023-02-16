b192c0d7ad339e526c1aa53a7a15408808ecf188
udhungel <udeepdhungel@gmail.com>
2023-02-16 20:59:52 +0100

    Identity Server - Adding DbContext and ApplicationUser cs file Renamed Main Module to no space

---
+++ Manga.Services.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Mango.Services.ProductAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Manga.Services.Identity.Models;
+
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+
+        }   
+
+    }
+}
